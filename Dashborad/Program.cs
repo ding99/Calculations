0b55164eac81512cb8dac9fee616489278362d0d
Wei Ding <ding_web@yahoo.co.jp>
2022-11-06 00:24:11 -0700

    add main

---
+++ Dashborad/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("= Start Calculation");
+
+var kernel = new Kernel.KCore();
+double a = 3.5, b = 2.5;
+Console.WriteLine($"{a} + {b} is {kernel.Add(a, b)}");
+
+Console.WriteLine("== End Calculation");
+Console.ResetColor();

ca7fdb9638b89260b60bc4117e4e514e23514ca4
Wei Ding <ding_web@yahoo.co.jp>
2022-11-06 00:42:22 -0700

    update calling

--- Dashborad/Program.cs
+++ Dashborad/Program.cs
@@ -1,9 +1,13 @@
-﻿Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("= Start Calculation");
+﻿Console.WriteLine("= Start Calculation");
 
 var kernel = new Kernel.KCore();
-double a = 3.5, b = 2.5;
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+double a = 3.5, b = 4;
 Console.WriteLine($"{a} + {b} is {kernel.Add(a, b)}");
 
-Console.WriteLine("== End Calculation");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"{a} * {b} is {kernel.Multiple(a, b)}");
+
 Console.ResetColor();
+Console.WriteLine("== End Calculation");
